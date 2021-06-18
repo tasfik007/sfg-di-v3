800b5681bea82725680076d1e24d135df8572894
Tasfik Rahman <tasfikrahman001@gmail.com>
2021-06-18 09:06:47 +0600

    first commit

---
+++ src/main/java/io/tasfik007/sfgdiv3/SfgDiV3Application.java
@@ -0,0 +1,13 @@
+package io.tasfik007.sfgdiv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgDiV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/io/tasfik007/sfgdiv3/SfgDiV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tasfik007.sfgdiv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
